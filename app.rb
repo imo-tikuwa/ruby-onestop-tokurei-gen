6298eecbcb09dec9918b214d95be54e1d501296b
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-01-06 00:37:27 +0900

    フォームのPOST内容をtlfテンプレートに埋め込み中／ビルトインサーバー起動時にRUBYOPTの指定を追記

--- app.rb
+++ app.rb
@@ -11,48 +11,58 @@ end
 # PDFをダウンロードするルーティング
 post '/download_pdf' do
 
-  test1 = Time.now.strftime("%Y年%m月%d日 %H時%M分%S秒")
   file_name = Time.now.strftime("%Y%m%d%H%M%S") + '_onestop.pdf'
   file_path = 'output/' + file_name
 
+  date = Date.today
+  current_year = date.strftime("%Y").to_i - 2018
+  current_month = date.strftime("%-m")
+  current_day = date.strftime("%-d")
+
   # PDF生成
-  Thinreports::Report.generate(:filename => file_path, :layout => 'tlfs/onestop.tlf') do
-    start_new_page
+  report = Thinreports::Report.new layout: 'tlfs/onestop.tlf'
+  report.start_new_page
 
-    # とりあえず決め打ちで意図した値が表示されることを確認
-    page.item(:top_current_year).value("2")
-
-    page.item(:current_year).value("2")
-    page.item(:current_month).value("1")
-    page.item(:current_day).value("5")
-    page.item(:mayor).value("てすと市長")
-    page.item(:zip).value("〒101-0032")
-    page.item(:address).value("東京都 千代田区\nほげほげ\nふがふがビル2F")
-    page.item(:phone_number).value("090-1111-2222")
-
-    page.item(:name_kana).value("イモ　チクワ")
-    page.item(:name).value("いも　ちくわ")
-    page.item(:my_number).value("123456789012")
-    page.item(:man).value("〇")
-    page.item(:woman).value("")
-
-    page.item(:year_meiji).value("")
-    page.item(:year_taisyo).value("")
-    page.item(:year_syowa).value("")
-    page.item(:year_heisei).value("〇")
-    page.item(:year_reiwa).value("")
-    page.item(:birth_year).value("1")
-    page.item(:birth_month).value("2")
-    page.item(:birth_day).value("12")
-
-    page.item(:donation_year).value("1")
-    page.item(:donation_month).value("12")
-    page.item(:donation_day).value("31")
-    page.item(:donation_amount).value('\10,000')
-
-    page.item(:check_one).value("\u2713")
-    page.item(:check_two).value("\u2713")
+  report.page.item(:top_current_year).value(current_year)
+  report.page.item(:current_year).value(current_year)
+  report.page.item(:current_month).value(current_month)
+  report.page.item(:current_day).value(current_day)
+  report.page.item(:mayor).value(params[:mayor])
+  report.page.item(:zip).value("〒" + params[:zip])
+  report.page.item(:address).value(params[:address])
+  report.page.item(:phone_number).value(params[:phone_number])
+  report.page.item(:name_kana).value(params[:name_kana])
+  report.page.item(:name).value(params[:name])
+  report.page.item(:my_number).value(params[:my_number])
+  if "man" == params[:sex] then
+    report.page.item(:man).value("〇")
+  elsif "woman" == params[:sex] then
+    report.page.item(:woman).value("")
   end
+
+  report.page.item(:year_meiji).value("")
+  report.page.item(:year_taisyo).value("")
+  report.page.item(:year_syowa).value("")
+  report.page.item(:year_heisei).value("〇")
+  report.page.item(:year_reiwa).value("")
+  report.page.item(:birth_year).value("1")
+  report.page.item(:birth_month).value("2")
+  report.page.item(:birth_day).value("12")
+
+  report.page.item(:donation_year).value("1")
+  report.page.item(:donation_month).value("12")
+  report.page.item(:donation_day).value("31")
+  report.page.item(:donation_amount).value('\10,000')
+
+  if "yes" == params[:check_one] then
+    report.page.item(:check_one).value("\u2713")
+  end
+  if "yes" == params[:check_two] then
+    report.page.item(:check_two).value("\u2713")
+  end
+
+  # PDF生成
+  report.generate filename: file_path
 
   # PDFダウンロード
   stat = File::stat(file_path)
--- server/server.bat
+++ server/server.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set RUBYOPT=-EUTF-8
+
 cd /D %~dp0
 cd ../
 bundle exec ruby app.rb -p 80
