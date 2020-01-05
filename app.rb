819741429b8ad5e609f2c0c207e2fca58c28e5dc
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-01-05 22:49:40 +0900

    thinreports動的出力用のアイテムをすべて埋込／テストデータで決め打ちで出力確認

--- app.rb
+++ app.rb
@@ -20,12 +20,38 @@ get '/download_pdf' do
     start_new_page
 
     # とりあえず決め打ちで意図した値が表示されることを確認
+    page.item(:top_current_year).value("2")
+
     page.item(:current_year).value("2")
     page.item(:current_month).value("1")
     page.item(:current_day).value("5")
-    page.item(:name).value("いも　ちくわ")
+    page.item(:mayor).value("てすと市長")
+    page.item(:zip).value("〒101-0032")
+    page.item(:address).value("東京都 千代田区\nほげほげ\nふがふがビル2F")
+    page.item(:phone_number).value("090-1111-2222")
+
     page.item(:name_kana).value("イモ　チクワ")
+    page.item(:name).value("いも　ちくわ")
     page.item(:my_number).value("123456789012")
+    page.item(:man).value("〇")
+    page.item(:woman).value("")
+
+    page.item(:year_meiji).value("")
+    page.item(:year_taisyo).value("")
+    page.item(:year_syowa).value("")
+    page.item(:year_heisei).value("〇")
+    page.item(:year_reiwa).value("")
+    page.item(:birth_year).value("1")
+    page.item(:birth_month).value("2")
+    page.item(:birth_day).value("12")
+
+    page.item(:donation_year).value("1")
+    page.item(:donation_month).value("12")
+    page.item(:donation_day).value("31")
+    page.item(:donation_amount).value('\10,000')
+
+    page.item(:check_one).value("\u2713")
+    page.item(:check_two).value("\u2713")
   end
 
   # PDFダウンロード
